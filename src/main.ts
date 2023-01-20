315d3ac28009437ea131515eb93a9a9e2903512d
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-01-20 12:59:29 -0300

    ignore all data that is not on dto class validator body

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // ignore all data that is not on dto class validator body
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
